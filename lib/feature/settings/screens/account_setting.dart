294f5af01c85d4456d1b9cfc046d08f959573188
Muhammed Zohaib Anwer <zohaibanwer2002@gmail.com>
2024-12-15 19:50:18 +0500

    Removed Scaffold from settings

--- lib/feature/settings/screens/account_setting.dart
+++ lib/feature/settings/screens/account_setting.dart
@@ -46,125 +46,116 @@ class _AccountScreenState extends State<AccountScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Ionicons.chevron_back_outline),
-        ),
-        leadingWidth: 80,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Settings",
-                style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Settings",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 40),
-              const Text(
-                "Account",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                ),
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              "Account",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
               ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    Image.asset("assets/images/avatar.png",
-                        width: 50, height: 50),
-                    const SizedBox(width: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          _fullname,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Image.asset("assets/images/avatar.png",
+                      width: 50, height: 50),
+                  const SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _fullname,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          _username,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
-                          ),
-                        )
-                      ],
-                    ),
-                    const Spacer(),
-                    ForwardButton(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const EditAccountScreen(),
-                          ),
-                        );
-                      },
-                    )
-                  ],
-                ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        _username,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  ForwardButton(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const EditAccountScreen(),
+                        ),
+                      );
+                    },
+                  )
+                ],
               ),
-              const SizedBox(height: 40),
-              const Text(
-                "Settings",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                ),
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              "Settings",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
               ),
-              const SizedBox(height: 20),
-              SettingItem(
-                title: "Language",
-                icon: Ionicons.earth,
-                bgColor: Colors.orange.shade100,
-                iconColor: Colors.orange,
-                value: "English",
-                onTap: () {},
-              ),
-              const SizedBox(height: 20),
-              SettingItem(
-                title: "Notifications",
-                icon: Ionicons.notifications,
-                bgColor: Colors.blue.shade100,
-                iconColor: Colors.blue,
-                onTap: () {},
-              ),
-              const SizedBox(height: 20),
-              SettingSwitch(
-                title: "Dark Mode",
-                icon: Ionicons.earth,
-                bgColor: Colors.purple.shade100,
-                iconColor: Colors.purple,
-                value: isDarkMode,
-                onTap: (value) {
-                  setState(() {
-                    isDarkMode = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 20),
-              SettingItem(
-                title: "Help",
-                icon: Ionicons.nuclear,
-                bgColor: Colors.red.shade100,
-                iconColor: Colors.red,
-                onTap: () {},
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            SettingItem(
+              title: "Language",
+              icon: Ionicons.earth,
+              bgColor: Colors.orange.shade100,
+              iconColor: Colors.orange,
+              value: "English",
+              onTap: () {},
+            ),
+            const SizedBox(height: 20),
+            SettingItem(
+              title: "Notifications",
+              icon: Ionicons.notifications,
+              bgColor: Colors.blue.shade100,
+              iconColor: Colors.blue,
+              onTap: () {},
+            ),
+            const SizedBox(height: 20),
+            SettingSwitch(
+              title: "Dark Mode",
+              icon: Ionicons.earth,
+              bgColor: Colors.purple.shade100,
+              iconColor: Colors.purple,
+              value: isDarkMode,
+              onTap: (value) {
+                setState(() {
+                  isDarkMode = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            SettingItem(
+              title: "Help",
+              icon: Ionicons.nuclear,
+              bgColor: Colors.red.shade100,
+              iconColor: Colors.red,
+              onTap: () {},
+            ),
+          ],
         ),
       ),
     );
